22314441420eed30df01da501c3f3a3b7f4ffe8c
Boyangggg <boyang-sun@outlook.com>
2024-06-01 10:23:29 +0200

    minor bug

--- utils/dataset_statistics.py
+++ utils/dataset_statistics.py
@@ -5,8 +5,8 @@ import numpy as np
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 
-# dir_path = Path("/cluster/project/cvg/boysun/Actmap_v2")
-dir_path = Path("/media/boysun/Extreme Pro/Actmap_v2_mini")
+dir_path = Path("/cluster/project/cvg/boysun/Actmap_v3")
+# dir_path = Path("/media/boysun/Extreme Pro/Actmap_v2_mini")
 dir_img = Path(dir_path / 'image/')
 dir_mask = Path(dir_path / 'weighted_mask/')
 dir_checkpoint = Path(dir_path / 'checkpoints/')
@@ -15,6 +15,7 @@ dir_depth = Path(dir_path / 'depth/')
 
 
 def get_npz_file_list_under_dir(dir_path):
+    print("getting npz files from dir: ", dir_path)
     npz_files = [f for f in listdir(dir_path) if isfile(join(dir_path, f)) and f.endswith('.npz')]
     return npz_files
 
@@ -36,7 +37,7 @@ if __name__ == "__main__":
         weighted_mask = load_weighted_mask_from_npz(dir_mask / npz_file)
         print("weighted_mask shape: ", weighted_mask.shape)
         mask_max_value_list.append(weighted_mask.max())
-        mask_mean_value_list.append(weighted_mask.mean())
+        mask_mean_value_list.append(weighted_mask[weighted_mask > 0].mean())
         global_max_value = max(mask_max_value_list)
 
     # plot the distribution of max value, x axis is the max value, y axis is the number of masks, interval is 100
