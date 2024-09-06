632dacf67590019be0f18d1ce3c36e563f4d4e10
boysun <boyang-sun@outlook.com>
2024-09-06 15:56:39 +0200

    minor sync

--- predict.py
+++ predict.py
@@ -13,7 +13,7 @@ from unet import UNet, UnetResnet, TwoHeadUnet
 from utils.utils import plot_img_and_mask
 import cv2
 import albumentations as A
-from utils.pred_to_line import df_to_linemap, df_wf_to_linemap, df_clsmask_to_linemap
+from utils.pred_to_line import df_to_linemap, df_wf_to_linemap, df_cls_to_line_weight
 import matplotlib.pyplot as plt
 
 def predict_img(net,
@@ -21,7 +21,6 @@ def predict_img(net,
                 depth_img,
                 device,
                 scale_factor = 0.5,
-                out_threshold=0.5,
                 use_depth=True,
                 img_size = [224,224]):
     
@@ -31,11 +30,11 @@ def predict_img(net,
     img = img.unsqueeze(0)
     img = img.to(device=device, dtype=torch.float32)
 
-    print("img size: ", img.size())
+    # print("img size: ", img.size())
     depth = torch.from_numpy(BasicDataset.preprocess(depth_img, scale_factor, is_mask=False, is_depth=True))
     depth = depth.unsqueeze(0)
     depth = depth.to(device=device, dtype=torch.float32)
-    print("depth size: ", depth.size())
+    # print("depth size: ", depth.size())
     
     # do a center crop to both image
     center_crop = transforms.CenterCrop((img_size[0], img_size[1]))
@@ -84,8 +83,20 @@ def get_args():
 
     return parser.parse_args()
 
+def resize_crop_img(img, scale, img_size):
+    # Resize the image
+    img = img.resize((int(img.width * scale), int(img.height * scale)), Image.BILINEAR)
 
-def create_pred_vis_overlay(line_map, img, scale):
+    # Center crop the image
+    center_crop = transforms.CenterCrop((img_size[0], img_size[1]))
+    img = center_crop(img)
+
+    return img
+
+def create_pred_vis_overlay(line_map, img, scale, non_interactive=False):
+
+    if non_interactive:
+        plt.switch_backend('agg')
 
     # Normalize the line_map to [0, 255] for heatmap
     line_map_normalized = cv2.normalize(line_map, None, 0, 255, cv2.NORM_MINMAX)
@@ -97,14 +108,10 @@ def create_pred_vis_overlay(line_map, img, scale):
     width, height = line_map_normalized.shape
 
     # Resize and center crop the image
-    print("img size: ", img.size)
     vis_img = img.resize((int(img.width * scale), int(img.height * scale)), Image.BILINEAR)
-    print("vis_img size: ", vis_img.size)
     center_crop = transforms.CenterCrop((width, height))
     vis_img = center_crop(vis_img)
     vis_img = np.array(vis_img)
-
-    print("vis_img size: ", vis_img.shape)
 
     # Convert BGR to RGB
     vis_img = cv2.cvtColor(vis_img, cv2.COLOR_BGR2RGB)
@@ -116,7 +123,6 @@ def create_pred_vis_overlay(line_map, img, scale):
     # Use matplotlib to create the colorbar and combine it with the image
     fig, ax = plt.subplots(figsize=(8, 8))
 
-    print("about to display plt")
     # Display the overlay image
     ax.imshow(overlay_img)
     ax.axis('off')  # Hide axes
@@ -140,123 +146,123 @@ def create_pred_vis_overlay(line_map, img, scale):
     img = img.reshape(fig.canvas.get_width_height()[::-1] + (4,))
     img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
 
-    print("about to close plt")
     plt.close()
 
     return img
 
 
-if __name__ == '__main__':
-    args = get_args()
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+# if __name__ == '__main__':
+#     args = get_args()
+#     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-    in_files = args.input
-    out_files = args.output
-    out_overlay_files = args.save_overlay_output
-    head_mode = args.head_mode
+#     in_files = args.input
+#     out_files = args.output
+#     out_overlay_files = args.save_overlay_output
+#     head_mode = args.head_mode
 
-    if args.use_depth:
-        # depth file is in the same directory as the image file but with a different name, depth
-        depth_files = in_files.replace('image', 'depth') if not args.use_mono_depth else in_files.replace('image', 'mono')
+#     if args.use_depth:
+#         # depth file is in the same directory as the image file but with a different name, depth
+#         depth_files = in_files.replace('image', 'depth') if not args.use_mono_depth else in_files.replace('image', 'mono')
 
-    if not os.path.exists(out_files):
-        os.makedirs(out_files)
-    if not os.path.exists(out_overlay_files):
-        os.makedirs(out_overlay_files)
+#     if not os.path.exists(out_files):
+#         os.makedirs(out_files)
+#     if not os.path.exists(out_overlay_files):
+#         os.makedirs(out_overlay_files)
 
-    if args.use_depth:  
-        print("Using depth images")
-        net = TwoHeadUnet(classes=args.classes,
-                            in_channels=4,
-                            head_config = head_mode,
-                            regression_downsample_factor=args.regression_downsample_factor)
+#     if args.use_depth:  
+#         print("Using depth images")
+#         net = TwoHeadUnet(classes=args.classes,
+#                             in_channels=4,
+#                             head_config = head_mode,
+#                             regression_downsample_factor=args.regression_downsample_factor)
         
-    else:
-        net = TwoHeadUnet(classes=args.classes,
-                            in_channels=3,
-                            head_config = head_mode,
-                            regression_downsample_factor=args.regression_downsample_factor)
+#     else:
+#         net = TwoHeadUnet(classes=args.classes,
+#                             in_channels=3,
+#                             head_config = head_mode,
+#                             regression_downsample_factor=args.regression_downsample_factor)
         
 
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    logging.info(f'Loading model {args.model}')
-    logging.info(f'Using device {device}')
+#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#     logging.info(f'Loading model {args.model}')
+#     logging.info(f'Using device {device}')
 
-    net.to(device=device)
-    state_dict = torch.load(args.model, map_location=device)
-    net.load_state_dict(state_dict)
+#     net.to(device=device)
+#     state_dict = torch.load(args.model, map_location=device)
+#     net.load_state_dict(state_dict)
 
-    logging.info('Model loaded!')
-    print("model num classes: ", net.n_classes)
+#     logging.info('Model loaded!')
+#     print("model num classes: ", net.n_classes)
 
-    # get all images in in_files folder with .jpg extension, into a list
-    print(in_files)
-    files = os.listdir(in_files)
-    in_imgs = []
-    out_imgs = []
-    out_overlay_imgs = []
+#     # get all images in in_files folder with .jpg extension, into a list
+#     print(in_files)
+#     files = os.listdir(in_files)
+#     in_imgs = []
+#     out_imgs = []
+#     out_overlay_imgs = []
 
-    for f in files:
-        if f.endswith('.jpg') or f.endswith('.png'):
-            in_imgs.append(os.path.join(in_files, f))
-            out_imgs.append(os.path.join(out_files, f))
-            out_overlay_imgs.append(os.path.join(out_overlay_files, f))
+#     for f in files:
+#         if f.endswith('.jpg') or f.endswith('.png'):
+#             in_imgs.append(os.path.join(in_files, f))
+#             out_imgs.append(os.path.join(out_files, f))
+#             out_overlay_imgs.append(os.path.join(out_overlay_files, f))
 
-    print(in_imgs)
+#     print(in_imgs)
 
     
 
-    for i, filename in enumerate(in_imgs):
-        logging.info(f'Predicting image {filename} ...')
-        img = Image.open(filename)
-        if args.use_depth:
-            depth_filename = os.path.join(depth_files, os.path.basename(filename).replace('img', 'depth'))
-            # depth is with png extension, not jpg, covert depth_filename to png
-            depth_filename = depth_filename.replace('.jpg', '.png')
-            depth = cv2.imread(depth_filename, cv2.IMREAD_GRAYSCALE)
+#     for i, filename in enumerate(in_imgs):
+#         logging.info(f'Predicting image {filename} ...')
+#         img = Image.open(filename)
+#         print("img size: ", img.size)
+#         if args.use_depth:
+#             depth_filename = os.path.join(depth_files, os.path.basename(filename).replace('img', 'depth'))
+#             # depth is with png extension, not jpg, covert depth_filename to png
+#             depth_filename = depth_filename.replace('.jpg', '.png')
+#             depth = cv2.imread(depth_filename, cv2.IMREAD_GRAYSCALE)
             
-        result = predict_img(net=net,
-                           full_img=img,
-                           depth_img=depth,
-                           scale_factor=args.scale,
-                           out_threshold=args.mask_threshold,
-                           device=device,
-                           use_depth=args.use_depth,
-                           img_size=[224,224])
+#         result = predict_img(net=net,
+#                            full_img=img,
+#                            depth_img=depth,
+#                            scale_factor=args.scale,
+#                            out_threshold=args.mask_threshold,
+#                            device=device,
+#                            use_depth=args.use_depth,
+#                            img_size=[320,320])
         
-        if head_mode == 'df':
-            # prediction is a distance field
-            df_pred = result[0].squeeze(0).squeeze(0).cpu().numpy()
-            line_map = df_to_linemap(df_pred, threshold=0.45)
-            # save the distance field as a heatmap image
-            out_filename = out_imgs[i]
-            # normalize the distance field to 0-255
-            df_pred = (df_pred - df_pred.min()) / (df_pred.max() - df_pred.min()) * 255
-            # convert the distance field to a heatmap image
-            heatmap = cv2.applyColorMap(np.uint8(df_pred), cv2.COLORMAP_VIRIDIS)
-            cv2.imwrite(out_filename, heatmap)
-            logging.info(f'Distance field saved to {out_filename}')
+#         if head_mode == 'df':
+#             # prediction is a distance field
+#             df_pred = result[0].squeeze(0).squeeze(0).cpu().numpy()
+#             line_map = df_to_linemap(df_pred, threshold=0.45)
+#             # save the distance field as a heatmap image
+#             out_filename = out_imgs[i]
+#             # normalize the distance field to 0-255
+#             df_pred = (df_pred - df_pred.min()) / (df_pred.max() - df_pred.min()) * 255
+#             # convert the distance field to a heatmap image
+#             heatmap = cv2.applyColorMap(np.uint8(df_pred), cv2.COLORMAP_VIRIDIS)
+#             cv2.imwrite(out_filename, heatmap)
+#             logging.info(f'Distance field saved to {out_filename}')
 
-            # print number of positive pixels in the line map
-            line_map[line_map == 1] = 255
-            out_overlay_filename = out_overlay_imgs[i]
-            cv2.imwrite(out_overlay_filename, line_map)
-            logging.info(f'Line map saved to {out_overlay_filename}')
+#             # print number of positive pixels in the line map
+#             line_map[line_map == 1] = 255
+#             out_overlay_filename = out_overlay_imgs[i]
+#             cv2.imwrite(out_overlay_filename, line_map)
+#             logging.info(f'Line map saved to {out_overlay_filename}')
         
 
-        if head_mode == "df_seg":
-            df_pred = result[0]
-            label_mask_pred = result[1]
+#         if head_mode == "df_seg":
+#             df_pred = result[0]
+#             label_mask_pred = result[1]
 
-            bin_edges = get_bin_edges(net.n_classes)
-            line_mask, line_map = df_clsmask_to_linemap(df=df_pred, cls_mask=label_mask_pred, 
-                                                        df_neighborhood=10, threshold=0.05,
-                                                        bin_edges=bin_edges)
+#             bin_edges = get_bin_edges(net.n_classes)
+#             line_mask, line_map = df_cls_to_line_weight(df=df_pred, cls_mask=label_mask_pred, 
+#                                                         df_neighborhood=10, threshold=0.1,
+#                                                         bin_edges=bin_edges)
             
-            overlay_img = create_pred_vis_overlay(line_map, img, args.scale)
-            cv2.imwrite(out_overlay_imgs[i], overlay_img)
-            logging.info(f'Overlay image saved to {out_overlay_imgs[i]}')
+#             overlay_img = create_pred_vis_overlay(line_map, img, args.scale)
+#             cv2.imwrite(out_overlay_imgs[i], overlay_img)
+#             logging.info(f'Overlay image saved to {out_overlay_imgs[i]}')
 
 
             # # Normalize the line_map to [0, 255] for heatmap
